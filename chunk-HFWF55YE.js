110fdbef1a3ff6bf33ab42360832911d6e95574d
Alexander Strelkov <alv.strelkov@gmail.com>
2026-01-22 18:19:52 +0400

    Auto-generated commit

---
+++ chunk-HFWF55YE.js
@@ -0,0 +1,1 @@
+import{ha as e,ib as t}from"./chunk-B4EV7BUP.js";var c=(()=>{class i{static \u0275fac=function(n){return new(n||i)};static \u0275mod=t({type:i});static \u0275inj=e({})}return i})();export{c as a};
